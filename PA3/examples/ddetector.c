16782f0d6db2cc337a863b05427fe4c1934bcf4f
JangJuMan <21700633@handong.edu>
2019-05-30 13:01:43 +0900

    matrix bug still exist

--- PA3/examples/ddetector.c
+++ PA3/examples/ddetector.c
@@ -6,12 +6,13 @@
 #include<pthread.h>
 #include<unistd.h>
 
-pthread_mutex_t MUTE = PTHREAD_MUTEX_INITIALIZER;
-//pthread_mutex_t MUTE2 = PTHREAD_MUTEX_INITIALIZER;
+static pthread_mutex_t MUTE = PTHREAD_MUTEX_INITIALIZER;
 
 static unsigned long int tid_log[10] = {0x0};
-static char* mutex_log[10][100] = {0x0};
-static char* adj_matrix[100][100] = {0};
+//static char* mutex_log[10][100] = {0x0};
+//static char* adj_matrix[100][100] = {0};
+static pthread_mutex_t* mutex_log[10][100] = {0};
+static pthread_mutex_t* adj_matrix[100][100] ={0};
 static int number_of_node = 0;
 static int edge_tail[100] = {0};
 static int new_check = 0;
@@ -22,6 +23,7 @@ static int adj_col=0;
 static int thread_count=0;	// mutex_count=0;
 static int curr[10] = {0};
 
+//for debug--------------------------------
 void print_curr(){
 	fprintf(stderr, "[curr]---------\n");
 	for(int i=0; i<10; i++){
@@ -41,7 +43,7 @@ void print_tid_log(){
 void print_mutex_log(){
 	fprintf(stderr, "[mutex_log]---------\n");
 	for(int i=0; i<5; i++){
-		for(int j=0; j<5; j++){
+		for(int j=0; j<10; j++){
 			fprintf(stderr, "[%p] ", mutex_log[i][j]);
 		}
 		fprintf(stderr, "\n");
@@ -51,23 +53,35 @@ void print_mutex_log(){
 
 void print_adj_mat(){
 	fprintf(stderr, "[adj_matrix]---------\n");
-		for(int i=0; i<5; i++){
-			for(int j=0; j<5; j++){
+		for(int i=0; i<10; i++){
+			for(int j=0; j<10; j++){
 				fprintf(stderr, "[%p] ", adj_matrix[i][j]);
 			}
 			fprintf(stderr, "\n");
 		}
-	fprintf(stderr, "\n\n");
+	fprintf(stderr, "\n");
 }
+
+void print_edge_tail(){
+	fprintf(stderr, "[edge_tail]----------\n");
+	for(int i=0; i<10; i++){
+		fprintf(stderr, "[%d] ", edge_tail[i]);
+	}
+	fprintf(stderr, "\n");
+}
+
 
 void print_all(){
 	print_curr();
 	print_tid_log();
 	print_mutex_log();
 	print_adj_mat();
+	print_edge_tail();
 }
 
+//----------------------------------------------
 
+//해당 쓰레드 아이디의 인덱스를 리턴/ 없다면 -1 리턴 
 int check_tid(unsigned long int tid){
 	for(int i=0; i<10; i++){
 		if(tid_log[i] == tid)
@@ -76,6 +90,7 @@ int check_tid(unsigned long int tid){
 	return -1;
 }
 
+// adj_matrix에서 첫째 줄에 노드 추가(lock)
 void node_check(pthread_mutex_t *mutex){
 	new_check = 0;
 	for(int i=0; i<100; i++){
@@ -96,6 +111,7 @@ void node_check(pthread_mutex_t *mutex){
 	}
 }
 
+// adj_matrix에 엣지 부분 추가하는 것 (lock)
 void edge_add(pthread_mutex_t *before, pthread_mutex_t *mutex){
 	for(int i=0; i<100; i++){
 		if(adj_matrix[i][0] == before){
@@ -105,7 +121,37 @@ void edge_add(pthread_mutex_t *before, pthread_mutex_t *mutex){
 	}
 }
 
+// adj_matrix에서 첫째 줄에 노드 제거 (unlock)
+void remove_node(pthread_mutex_t *mutex){
+	for(int i=0; i<100; i++){
+		printf("adj_matrix[%d][0] == %p == mutex(%p)\n",i, adj_matrix[i][0], mutex);
+		if(adj_matrix[i][0] == mutex){
+			adj_matrix[i][0] = 0;
+			//??
+	//		edge_tail[i] = 0;
+			break;
+		}
+	}
+}
 
+
+// adj_matrix에서 엣지 제거 (unlock)
+void remove_edge(pthread_mutex_t *before, pthread_mutex_t *mutex){
+	for(int i=0; i<100; i++){
+		if(adj_matrix[i][0] == before){
+			fprintf(stderr, "<< is same?\n");
+			if(adj_matrix[i][edge_tail[i]] == mutex){
+				fprintf(stderr, ">> same\n");
+				adj_matrix[i][edge_tail[i]--] = 0;
+			}
+			break;
+		}
+	}
+	remove_node(mutex);
+}
+
+
+// lock을 overriding
 int pthread_mutex_lock(pthread_mutex_t *mutex){
 	
 	int (*pthread_mutex_lock_p)(pthread_mutex_t *mutex);
@@ -120,7 +166,7 @@ int pthread_mutex_lock(pthread_mutex_t *mutex){
 	//LOCK+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 	pthread_mutex_lock_p(&MUTE);
 	pthread_t tid = pthread_self();
-	//fprintf(stderr, "Starting of pthread_mutex_lock()==================================\n");
+	fprintf(stderr, "Starting of pthread_mutex_lock(%p)==================================\n", mutex);
 
 		// 처음오는 스레드이면 새롭게 추가하고/ 아니면 기존 거에 추가한다.
 		int now = check_tid(tid);
@@ -130,8 +176,6 @@ int pthread_mutex_lock(pthread_mutex_t *mutex){
 		}
 		mutex_log[now][curr[now]++] = mutex;
 
-		int ptr = pthread_mutex_lock_p(mutex);
-
 		// add at first colum
 		node_check(mutex);
 		for(int i=0; i< curr[now]; i++){
@@ -140,17 +184,19 @@ int pthread_mutex_lock(pthread_mutex_t *mutex){
 		}
 
 
-	//	print_all();
+		print_all();
 	//UNLOCK+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 	pthread_mutex_unlock_p(&MUTE);
-	//fprintf(stderr, "Ending of pthread_mutex_lock()===================================\n");
+	fprintf(stderr, "Ending of pthread_mutex_lock()===================================\n\n\n");
 
-	return ptr;
+	return pthread_mutex_lock_p(mutex);
 }
 
 
+// unlock 을 overriding
 int pthread_mutex_unlock(pthread_mutex_t *mutex){
 
+	int erase_check = 0;
 	int (*pthread_mutex_lock_p)(pthread_mutex_t *mutex);
 	int (*pthread_mutex_unlock_p)(pthread_mutex_t *mutex);
 	char* error;
@@ -163,20 +209,44 @@ int pthread_mutex_unlock(pthread_mutex_t *mutex){
 	//LOCK+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 	pthread_mutex_lock_p(&MUTE);
 	pthread_t tid = pthread_self();
-	//fprintf(stderr, "Starting of pthread_mutex_unlock()+++++++++++++++++++++++++++++++++++\n");
+	fprintf(stderr, "Starting of pthread_mutex_unlock(%p)+++++++++++++++++++++++++++++++++++\n", mutex);
 
 		// 일치하는 스레드에서 해당 뮤택스를 지운다
 		int now = check_tid(tid);
 		mutex_log[now][--curr[now]] = 0;
 
-		int ptr = pthread_mutex_unlock_p(mutex);
+		for(int i=0; i<100; i++){
+			for(int j=1; j<100; j++){
+				if(i == now){	// || j == (curr[now] + 1)){
+//					fprintf(stderr, "i(%d) == now(%d) || j(%d) == curr[%d] + 1 == %d\n", i, now, j, now, curr[now] + 1);
+				}
+				else{
+					if(adj_matrix[i][j] == mutex){
+						erase_check++;
+//						fprintf(stderr, "erase_check : %d\n", erase_check);
+					}
+				}
+			}
+		}
+		printf("erase ch : %d, curr[now] : %d\n",erase_check, curr[now]);
+		if(erase_check == 0){
+			if(curr[now] == 0){
+				remove_node(mutex);
+			}
+			else{
+				for(int i=0; i<curr[now]; i++){
+					remove_edge(mutex_log[now][i],mutex);
+				}
+			}
+		}
 
-	//	print_all();
+
+		print_all();
 
 	//UNLOCK+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 	pthread_mutex_unlock_p(&MUTE);
-	//fprintf(stderr, "Ending of pthread_mutex_unlock()+++++++++++++++++++++++++++++++++++++\n");
+	fprintf(stderr, "Ending of pthread_mutex_unlock()+++++++++++++++++++++++++++++++++++++\n\n\n");
 
-	return ptr;
+	return pthread_mutex_unlock_p(mutex);
 }
 
